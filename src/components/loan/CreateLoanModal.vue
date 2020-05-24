3c3c90f2666fd31918aa76b01640a4f2200bfa39
pgolez <paul.golez@codingavenue.com>
2020-05-24 20:45:34 +0800

    Extract modal for creating loan to another component

---
+++ src/components/loan/CreateLoanModal.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-dialog v-model="dialog" max-width="500px">
+    <template v-slot:activator="{ on }">
+      <v-btn color="primary" dark class="mb-2" v-on="on">
+        Draft a new loan
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">New Loan</span>
+      </v-card-title>
+      <v-card-text>
+        <v-form ref="form">
+          <BorrowerSelect />
+          <v-text-field
+            v-model="loan.amount"
+            label="Amount"
+            outlined
+            required>
+          </v-text-field>
+          <v-text-field
+            v-model="loan.installmentCount"
+            label="Number of Installments"
+            type="number"
+            outlined
+            required>
+          </v-text-field>
+          <v-text-field
+            v-model="loan.interestRate"
+            label="Monthly Interest Rate"
+            type="number"
+            disabled
+            outlined
+            required>
+          </v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions class="pb-6 pr-6">
+        <v-spacer></v-spacer>
+        <v-btn outlined @click="close">Cancel</v-btn>
+        <v-btn color="primary">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import BorrowerSelect from './LoanFormBorrowerSelect';
+
+export default {
+  components: {
+    BorrowerSelect
+  },
+  data() {
+    return {
+      dialog: false,
+      loan: {
+        borrower: "",
+        amount: null,
+        installmentCount: 2,
+        interestRate: 0.12,
+      }
+    };
+  },
+  methods: {
+    close() {
+      this.dialog = false;
+    }
+  }
+}
+</script>
--- src/components/loan/LoanPage.vue
+++ src/components/loan/LoanPage.vue
@@ -14,50 +14,8 @@
            <template v-slot:top>
             <v-toolbar flat color="white">
               <v-spacer></v-spacer>
-              <v-dialog v-model="dialog" max-width="500px">
-                <template v-slot:activator="{ on }">
-                  <v-btn color="primary" dark class="mb-2" v-on="on">
-                    Draft a new loan
-                  </v-btn>
-                </template>
-                <v-card>
-                  <v-card-title>
-                    <span class="headline">New Loan</span>
-                  </v-card-title>
-                  <v-card-text>
-                    <v-form ref="form">
+              <CreateLoanModal />
 
-                      <BorrowerSelect />
-                        <v-text-field
-                          v-model="loan.amount"
-                          label="Amount"
-                          outlined
-                          required>
-                         </v-text-field>
-                        <v-text-field
-                          v-model="loan.installmentCount"
-                          label="Number of Installments"
-                          type="number"
-                          outlined
-                          required>
-                         </v-text-field>
-                        <v-text-field
-                          v-model="loan.interestRate"
-                          label="Monthly Interest Rate"
-                          type="number"
-                          disabled
-                          outlined
-                          required>
-                         </v-text-field>
-                    </v-form>
-                  </v-card-text>
-                  <v-card-actions class="pb-6 pr-6">
-                    <v-spacer></v-spacer>
-                    <v-btn outlined>Cancel</v-btn>
-                    <v-btn color="primary">Save</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
             </v-toolbar>
 
           </template>
@@ -78,12 +36,12 @@
 </template>
 
 <script>
-import BorrowerSelect from './LoanFormBorrowerSelect';
+import CreateLoanModal from './CreateLoanModal';
 import { LoanRepository } from "@/repositories/repository";
 
 export default {
   components: {
-    BorrowerSelect
+    CreateLoanModal
   },
   data() {
     return {
@@ -96,7 +54,6 @@ export default {
         { text: "Status", sortable: true, value: "paid_amount" }
       ],
       loans: [],
-      dialog: false,
       loan: {
         borrower: "",
         amount: null,
