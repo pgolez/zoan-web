9c978e9ebf42deb36d9f2b94b62d273fb7a70bd3
pgolez <paul.golez@codingavenue.com>
2020-05-24 15:56:29 +0800

    Add initial loans page layout

--- src/components/loan/LoanPage.vue
+++ src/components/loan/LoanPage.vue
@@ -1,7 +1,55 @@
 <template>
-  <div>LoanPage</div>
+  <v-container fluid>
+    <v-row dense>
+      <v-col>
+        <div>fucking breadcrumbs here</div>
+      </v-col>
+    </v-row>
+    <v-row dense>
+      <v-col :cols="12">
+        <v-card>
+          <v-card-title>Loans</v-card-title>
+          <v-data-table :headers="headers" :items="loans" class="elevation-1">
+            <template v-slot:item.monthlyInterest="{ item }">
+              <span>{{ `${item.monthlyInterest * 100}%` }}</span>
+            </template>
+
+            <template v-slot:item.paid_amount="{ item }">
+              <v-progress-linear :value="loanProgress(item)" rounded color="red accent-4"></v-progress-linear>
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+import LoanRepository from "@/repositories/loan.repository";
+
+export default {
+  data() {
+    return {
+      headers: [
+        { text: "Borrower", sortable: true, value: "borrower.name" },
+        { text: "Date Loaned", sortable: true, value: "date_loaned" },
+        { text: "Interest", sortable: true, value: "monthlyInterest" },
+        { text: "Amount", sortable: true, value: "amount" },
+        { text: "Status", sortable: true, value: "paid_amount" }
+      ],
+      loans: []
+    };
+  },
+  created() {
+    this.fetchLoans();
+  },
+  methods: {
+    async fetchLoans() {
+      this.loans = await LoanRepository.list();
+    },
+    loanProgress(loan) {
+      return (loan.paid_amount / loan.amount) * 100;
+    }
+  }
+};
 </script>
