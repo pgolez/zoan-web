c649d0a3ed3f7bcf0de95dc0ee7fb5586fddfa78
pgolez <paul.golez@codingavenue.com>
2020-05-24 21:29:52 +0800

    Create new loan

--- src/repositories/loan.repository.js
+++ src/repositories/loan.repository.js
@@ -1,10 +1,23 @@
 import client from './client'
 
+const BASE_URL = '/api/loans'
+
 export default {
 
     async list() {
         const loans = await client.get('/api/loans');
         return loans;
+    },
+
+    async create(loan) {
+        const data = {
+            amount: loan.amount,
+            borrowerId: loan.borrowerId,
+            installmentCount: loan.installmentCount,
+            monthlyInterest: loan.monthlyInterest
+        }
+        await client.post(BASE_URL, data)
+        return loan
     }
 
 };
