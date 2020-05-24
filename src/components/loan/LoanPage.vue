8221428986759d941f5b84be05ba9316a625f216
pgolez <paul.golez@codingavenue.com>
2020-05-24 19:52:22 +0800

    Initial draft loan form

--- src/components/loan/LoanPage.vue
+++ src/components/loan/LoanPage.vue
@@ -20,6 +20,7 @@
                 vertical
               ></v-divider>
               <v-spacer></v-spacer>
+
               <v-dialog v-model="dialog" max-width="500px">
                 <template v-slot:activator="{ on }">
                   <v-btn color="primary" dark class="mb-2" v-on="on">
@@ -30,42 +31,52 @@
                   <v-card-title>
                     <span class="headline">New Loan</span>
                   </v-card-title>
-
-                  <!-- <v-card-text>
-                    <v-container>
-                      <v-row>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                        </v-col>
-                      </v-row>
-                    </v-container>
-                  </v-card-text> -->
-
-                  <!-- <v-card-actions>
+                  <v-card-text>
+                    <v-form ref="form">
+                      <v-text-field
+                        v-model="loan.borrower"
+                        label="Borrower"
+                        outlined
+                        required>
+                       </v-text-field>
+                        <v-text-field
+                          v-model="loan.amount"
+                          label="Amount"
+                          outlined
+                          required>
+                         </v-text-field>
+                        <v-text-field
+                          v-model="loan.installmentCount"
+                          label="Number of Installments"
+                          type="number"
+                          outlined
+                          required>
+                         </v-text-field>
+                        <v-text-field
+                          v-model="loan.interestRate"
+                          label="Monthly Interest Rate"
+                          type="number"
+                          outlined
+                          required>
+                         </v-text-field>
+                        <v-text-field
+                          v-model="loan.fund"
+                          label="Fund Source"
+                          outlined
+                          required>
+                         </v-text-field>
+                    </v-form>
+                  </v-card-text>
+                  <v-card-actions class="pb-6 pr-6">
                     <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                    <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-                  </v-card-actions> -->
+                    <v-btn outlined>Cancel</v-btn>
+                    <v-btn color="primary">Save</v-btn>
+                  </v-card-actions>
                 </v-card>
               </v-dialog>
             </v-toolbar>
+
           </template>
-
-
-
-
             <template v-slot:item.monthlyInterest="{ item }">
               <span>{{ `${item.monthlyInterest * 100}%` }}</span>
             </template>
@@ -98,6 +109,13 @@ export default {
       ],
       loans: [],
       dialog: false,
+      loan: {
+        borrower: "",
+        amount: null,
+        installmentCount: 2,
+        interestRate: 0.12,
+        fund: ""
+      }
     };
   },
   computed: {
