a84220141237e9e1b49ecad2239f35e470d5481a
pgolez <paul.golez@codingavenue.com>
2020-05-30 19:08:59 +0800

    Create payment repository

--- src/repositories/repository.js
+++ src/repositories/repository.js
@@ -1,9 +1,11 @@
 import BorrowerRepository from './borrower.repository'
 import FundRepository from './fund.repository'
 import LoanRepository from './loan.repository'
+import PaymentRepository from './payment.repository'
 
 export {
     BorrowerRepository,
     FundRepository,
-    LoanRepository
+    LoanRepository,
+    PaymentRepository
 }
