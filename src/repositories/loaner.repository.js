9571da68a58de02775c918f24e54cac8f106fc60
pgolez <paul.golez@codingavenue.com>
2020-06-11 23:10:28 +0800

    Create method on loaner repository to save transaction

--- src/repositories/loaner.repository.js
+++ src/repositories/loaner.repository.js
@@ -1,4 +1,5 @@
 import client from './client'
+import _ from 'lodash'
 
 const BASE_URL = '/api/loaners'
 
@@ -13,5 +14,13 @@ export default {
         const data = { name: loaner.name }
         const savedLoaner = await client.post(BASE_URL, data)
         return savedLoaner;
+    },
+
+    async postTransaction(loaner, transaction) {
+        const data = _.pick(transaction, ['amount', 'type', 'transactionDate', 'notes'])
+        const url = `${BASE_URL}/${loaner.id}/equity/transactions`
+
+        const savedTransaction = await client.post(url, data)
+        return savedTransaction
     }
 };
